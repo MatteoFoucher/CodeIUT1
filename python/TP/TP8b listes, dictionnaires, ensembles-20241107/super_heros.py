7b6ece91fb0685fd59544abe17b17ab73519c8eb
MatteoFoucher <matteo.foucher@etu.univ-orleans.fr>
2024-11-07 14:59:26 +0100

    commit

---
+++ python/TP/TP8b listes, dictionnaires, ensembles-20241107/super_heros.py
@@ -0,0 +1,2 @@
+avengers = {'Spiderman': (5 , 5 , 'araignée a quatre pattes') , 'Hulk': (7, 4, "Grand homme vert") ,'Agent 13': (2, 3, 'agent 13') , 'M Melin': (2, 6, 'expert en archi')}
+def intelligence
