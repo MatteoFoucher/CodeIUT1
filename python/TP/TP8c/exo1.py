f1d50121f8c84002fc4aa30c532921bf5129cf10
MatteoFoucher <matteo.foucher@etu.univ-orleans.fr>
2024-11-14 10:53:56 +0100

    zelizegigzel

--- python/TP/TP8c/exo1.py
+++ python/TP/TP8c/exo1.py
@@ -26,5 +26,57 @@ def cumuler_jusqu_a_seuil(dico, seuil):
 #print(cumuler_jusqu_a_seuil(dico, 20))
 
 #Exo2
+courses = {"lait": 2.30, "pomme": 1, "yaourt": 5}
 
+def ajouter_article():
+    article = input("Entrez le nom de l'article que vous voulez ajouter: ")
+    prix = input("Entrez le prix de l'article: ")
+    courses[article] = float(prix)
+    print(courses)
+    return courses
+
+#ajouter_article()
+
+def suppr_article():
+    article = input("Entrez le nom de l'article que vous voulez supprimer: ")
+    courses.pop(article)
+    print(courses)
+    return courses
+#suppr_article()
+
+def modifier_article():
+    article = input("Entrez le nom de l'article que vous voulez modifier: ")
+    prix = input("Entrez le nouveau prix de l'article: ")
+    courses[article] = prix
+    print(courses)
+    return courses
+#modifier_article()
+
+def prix_total_courses(courses):
+    prix_total = 0
+    for prix in courses.values():
+        prix_total += prix
+    return prix_total
+#print(prix_total_courses(courses))
+
+def plus_cher(courses):
+    plus_cher = ""
+    prix_max = 0
+    for article in courses.keys():
+        if courses[article] >= prix_max:
+            prix_max = courses[article]
+            plus_cher = article
+    return plus_cher
+#print(plus_cher(courses))
+
+#Exo3
+
+def affichage_menu(titre):
+    print("+----------------------------+")
+    print("|Bienvenu sur" + titre + "|")
+    print("+----------------------------+")
+
+def programme_principal(courses):
+    running = True
+    affichage_menu("")
 
