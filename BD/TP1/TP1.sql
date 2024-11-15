940c1a491372896184cda604126fe81f50983c87
MatteoFoucher <matteo.foucher@etu.univ-orleans.fr>
2024-11-15 10:07:22 +0100

    comm blehefefuhzefuh

--- BD/TP1/TP1.sql
+++ BD/TP1/TP1.sql
@@ -8,12 +8,44 @@ create table CLIENT(
     nomCl varchar2(40),
     prenom varchar2(30),
     adresse varchar(50),
-    email varchar(50)
-)
+    email varchar(50),
+    constraint CleCLIENT PRIMARY KEY (codeCl)
+);
 
 insert into CLIENT values(12, 'Foucher', 'Matteo', '194 Avenue de lArgonne', 'matteofoucher@gmail.com');
 
 create table ARTICLE(
-    RefArt number(5).
-    designation 
-)
+    RefArt number(5),
+    designation varchar2(20),
+    prix number(5.2),
+    TVA number(4.2),
+    qteStock number(5.2),
+    
+
+);
+
+create table COMMANDE(
+    NumCom number(5),
+    DateCom date,
+    codeCl number(5)
+);
+
+create table COLIS(
+    Numcom number(5),
+    NumColis number(5),
+    indiceRetrait varchar2(20)
+);
+
+create table EXPEDIER(
+    NumCom number(5),
+    NumColis number(5),
+    RefArt number(5),
+    qteExp number(5),
+    qteAcc number(5)
+);
+
+create table CONTENIR(
+    numCom number(5),
+    RefArt number(5),
+    qteCom number(5)
+);
