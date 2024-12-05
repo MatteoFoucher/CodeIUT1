6edfb48bf8730194aafe2c65480b891d096db6bc
MatteoFoucher <matteo.foucher@etu.univ-orleans.fr>
2024-12-05 10:59:31 +0100

    commit

--- BD/TP3/schema.sql
+++ BD/TP3/schema.sql
@@ -71,23 +71,67 @@ select Nom
 from CLIENTS
 where Ville != 'Paris';
 --10. Lister les clients qui partent de Paris, mais qui n’habitent pas Paris.
-select Nom
+select distinct Nom
 from CLIENTS natural join VOYAGES natural join RESERVATIONS
 where VilleDepart = 'Paris' and Ville != VilleDepart;
 --11. Trouver les clients qui n’ont aucune r´eservation.
-select *
-from CLIENTS
-where ID not in (select ID from RESERVATIONS);
+select Nom, Prenom
+from Clients
+minus
+select Nom, Prenom
+from CLIENTS natural join RESERVATIONS;
 --12. Voyages qui ne font pas objet d’une r´eservation.
+select Code, VilleDepart, VilleArrivee
+from VOYAGES
+minus
+select Code, VilleDepart, VilleArrivee
+from VOYAGES natural join RESERVATIONS;
 --13. Clients qui partent de Paris, mais qui vont uniquement a Amsterdam (´eventuellement
 --plusieurs fois). Aucun voyage vers une autre destination.
+select distinct Nom, Prenom
+from CLIENTS natural join VOYAGES natural join RESERVATIONS
+where VilleDepart = 'Paris' and VilleArrivee = 'Amsterdam'
+minus
+select distinct Nom, Prenom
+from CLIENTS natural join VOYAGES natural join RESERVATIONS
+where VilleDepart = 'Paris' and VilleArrivee != 'Amsterdam';
+
 --14. Clients qui vont `a Amsterdam et `a Rio de Janeiro.
+select Nom
+from CLIENTS natural join VOYAGES natural join RESERVATIONS
+where VilleArrivee = 'Rio de Janeiro'
+intersect
+select Nom
+from CLIENTS natural join VOYAGES natural join RESERVATIONS
+where VilleArrivee = 'Amsterdam'; 
 --15. Clients qui vont `a Amsterdam ou `a Rio de Janeiro.
+select Nom
+from CLIENTS natural join VOYAGES natural join RESERVATIONS
+where VilleArrivee = 'Rio de Janeiro'
+union
+select Nom
+from CLIENTS natural join VOYAGES natural join RESERVATIONS
+where VilleArrivee = 'Amsterdam';
 --16. Couples de clients habitant la mˆeme ville. Indiquer la ville en question.
+select c1.Nom, c2.Nom
+from CLIENTS c1, CLIENTS c2
+where c1.Ville = c2.Ville and c1.Nom < c2.Nom;
 --17. Couples de (code) voyages ayant le mˆeme prix. Indiquer le prix en question.
+select c1.Prix, c2.Prix
+from VOYAGES c1, VOYAGES c2
+where c1.Prix = c2.Prix and c1.Prix < c2.Prix;
 --18. Clients ayant au moins deux r´eservations.
+select distinct c1.Nom
+from CLIENTS c1, RESERVATIONS R1, RESERVATIONS R2
+where R1.id = C1.id and R2.id = C1.id and R1.Code != R2.Code;
 --19. Clients ayant une seule r´eservation.
---page 1 sur 2
+select Nom
+from CLIENTS natural join RESERVATIONS
+minus
+select distinct c1.Nom
+from CLIENTS c1, RESERVATIONS R1, RESERVATIONS R2
+where R1.id = C1.id and R2.id = C1.id and R1.Code != R2.Code;
 --20. Couples de clients ayant fait des r´eservations le mˆeme jour. Informer aussi les codes
+
 --des voyages en question et la date de r´eservation.
 --21. Couples de clients ayant r´eserv´e le mˆeme voyage le mˆeme jour.
--- python/TP/TP10/pokedex/pokedex.py
+++ python/TP/TP10/pokedex/pokedex.py
@@ -103,14 +103,34 @@ def nombre_de_v2(attaque, pokedex):
     resultat: renvoie le nombre de pokemons de ce type d'attaque
     dans le pokedex
     """
-    
+    cpt_poke = 0
+    for poke in pokedex.values():
+        if attaque in poke:
+            cpt_poke += 1 
+    return cpt_poke
+#print(nombre_de_v2('Dragon', pokedex_anakin_v2))
+
 
 
 def attaque_preferee_v2(pokedex):
     """
     Renvoie le nom du type d'attaque qui est la plus fréquente dans le pokedex
     """
-    ...
+    dict_attaque = dict()
+    attaque_max = 0
+    for attaque in pokedex.values():
+        for type in attaque:
+            if type in dict_attaque.keys():
+                dict_attaque[type] += 1
+            else:
+                dict_attaque[type] = 1
+    for attaque, nb_attaques in dict_attaque.items():
+        if nb_attaques >= attaque_max:
+            attaque_max = nb_attaques
+            nom_attaque_max = attaque
+    return nom_attaque_max
+#print(attaque_preferee_v2(pokedex_anakin_v2))         
+    
 
 # =====================================================================
 # Modélisation n°3
@@ -118,10 +138,15 @@ def attaque_preferee_v2(pokedex):
 
 # Penser à completer la fonction exemples_pokedex_v3 dans le fichier de tests
 
+pokedex_anakin_v3 = {'Dragon': {'Carmache', 'Colimucus', 'Palkia'}, 'Sol': {'Carmache'}, 'Eau': {'Palkia'}}
 
 def appartient_v3(pokemon, pokedex):
     """ renvoie True si pokemon (str) est présent dans le pokedex """
-    ...
+    for poke in pokedex.values():
+        if pokemon in poke:
+            return True
+    return False
+#print(appartient_v3('evoli', pokedex_anakin_v3))
 
 
 def toutes_les_attaques_v3(pokemon, pokedex):
@@ -129,7 +154,12 @@ def toutes_les_attaques_v3(pokemon, pokedex):
     param: un pokedex et le nom d'un pokemon (str) qui appartient au pokedex
     resultat: renvoie l'ensemble des types d'attaque du pokemon passé en paramètre
     """
-    ...
+    attaques_pokemon = set()
+    for attaque, poke in pokedex.items():
+        if pokemon in poke:
+            attaques_pokemon.add(attaque)
+    return attaques_pokemon
+#print(toutes_les_attaques_v3('Palkia', pokedex_anakin_v3))
 
 
 def nombre_de_v3(attaque, pokedex):
@@ -138,14 +168,28 @@ def nombre_de_v3(attaque, pokedex):
     resultat: renvoie le nombre de pokemons de ce type d'attaque
     dans le pokedex
     """
-    ...
+    cpt_pokemon = 0
+    for type in pokedex.keys():
+        if type == attaque:
+            cpt_pokemon += len(pokedex[type])
+    return cpt_pokemon
+#print(nombre_de_v3('Sol', pokedex_anakin_v3))
+        
 
 
 def attaque_preferee_v3(pokedex):
     """
     Renvoie le nom du type d'attaque qui est la plus fréquente dans le pokedex
     """
-    ...
+    nom_attaque_max = ''
+    attaque_max = 0
+ 
+    for attaque, pokemon in pokedex.items():
+        if len(pokemon) >= attaque_max:
+            attaque_max = len(pokemon)
+            nom_attaque_max = attaque
+    return nom_attaque_max
+#print(attaque_preferee_v3(pokedex_anakin_v3))
 
 # =====================================================================
 # Transformations
@@ -158,7 +202,8 @@ def v1_to_v2(pokedex_v1):
     param: prend en paramètre un pokedex version 1
     renvoie le même pokedex mais en version 2
     """
-    ...
+    
+    
 
 
 # Version 1 ==> Version 2
